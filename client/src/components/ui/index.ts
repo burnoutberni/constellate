741d82997e2af28456c1e0aa58fd008111fd281d
Bernhard Hayden <hello@bhayden.at>
2025-12-09 23:31:53 +0100

    Sort barrel file alphabetically

--- client/src/components/ui/index.ts
+++ client/src/components/ui/index.ts
@@ -5,9 +5,15 @@
  * and dark mode support.
  */
 
+export {
+  Avatar,
+  AvatarGroup,
+  type AvatarProps,
+  type AvatarSize,
+  type AvatarGroupProps,
+} from './Avatar'
+export { Badge, type BadgeProps, type BadgeVariant, type BadgeSize } from './Badge'
 export { Button, type ButtonProps, type ButtonVariant, type ButtonSize } from './Button'
-export { Input, type InputProps, type InputSize } from './Input'
-export { Textarea, type TextareaProps, type TextareaSize } from './Textarea'
 export {
   Card,
   CardHeader,
@@ -21,11 +27,5 @@ export {
   type CardContentProps,
   type CardFooterProps,
 } from './Card'
-export { Badge, type BadgeProps, type BadgeVariant, type BadgeSize } from './Badge'
-export {
-  Avatar,
-  AvatarGroup,
-  type AvatarProps,
-  type AvatarSize,
-  type AvatarGroupProps,
-} from './Avatar'
+export { Input, type InputProps, type InputSize } from './Input'
+export { Textarea, type TextareaProps, type TextareaSize } from './Textarea'
