5da6514ef68f92851cd27a452b94b365f9e34f61
Bernhard Hayden <hello@bhayden.at>
2025-12-15 21:21:59 +0100

    Fix suggestions

--- client/src/components/ui/SafeHTML.tsx
+++ client/src/components/ui/SafeHTML.tsx
@@ -1,13 +1,16 @@
 import DOMPurify from 'dompurify'
 import { useMemo, type ElementType } from 'react'
 
+// Regular expression to match external URLs (http://, https://, or protocol-relative //)
+const EXTERNAL_URL_REGEX = /^(https?:\/\/|\/\/)/i
+
 // Add hook once at module level to modify external links during sanitization
 // This runs synchronously during sanitization, avoiding hydration mismatches
 DOMPurify.addHook('afterSanitizeAttributes', (node) => {
 	// Add target="_blank" and rel="noopener noreferrer" to external links
 	if (node.tagName === 'A' && node.hasAttribute('href')) {
 		const href = node.getAttribute('href')
-		if (href && (href.startsWith('http://') || href.startsWith('https://'))) {
+		if (href && EXTERNAL_URL_REGEX.test(href)) {
 			node.setAttribute('target', '_blank')
 			node.setAttribute('rel', 'noopener noreferrer')
 		}
@@ -29,6 +32,42 @@ export interface SafeHTMLProps {
 	tag?: ElementType
 }
 
+// DOMPurify configuration for safe HTML sanitization
+// This allows common formatting tags like <p>, <strong>, <em>, <br>, <a>, etc.
+// but blocks dangerous tags like <script>, <iframe>, etc.
+// The afterSanitizeAttributes hook (added at module level) will automatically
+// add target="_blank" and rel="noopener noreferrer" to external links
+const DOMPURIFY_CONFIG = {
+	ALLOWED_TAGS: [
+		'p',
+		'br',
+		'strong',
+		'em',
+		'u',
+		's',
+		'strike',
+		'del',
+		'h1',
+		'h2',
+		'h3',
+		'h4',
+		'h5',
+		'h6',
+		'ul',
+		'ol',
+		'li',
+		'a',
+		'blockquote',
+		'pre',
+		'code',
+		'span',
+		'div',
+	],
+	ALLOWED_ATTR: ['href', 'title', 'target', 'rel'],
+	ALLOWED_URI_REGEXP:
+		/^(?:(?:(?:f|ht)tps?|mailto|tel|callto|sms|cid|xmpp):|[^a-z]|[a-z+\-.]+(?:[^a-z+.\-:]|$))/i,
+}
+
 /**
  * SafeHTML component renders HTML content after sanitizing it with DOMPurify
  * to prevent XSS attacks. Only safe HTML tags and attributes are allowed.
@@ -44,43 +83,7 @@ export function SafeHTML({ html, className, tag: Tag = 'div' }: SafeHTMLProps) {
 			return ''
 		}
 
-		// Configure DOMPurify to allow safe HTML tags and attributes
-		// This allows common formatting tags like <p>, <strong>, <em>, <br>, <a>, etc.
-		// but blocks dangerous tags like <script>, <iframe>, etc.
-		// The afterSanitizeAttributes hook (added at module level) will automatically
-		// add target="_blank" and rel="noopener noreferrer" to external links
-		const sanitized = DOMPurify.sanitize(html, {
-			ALLOWED_TAGS: [
-				'p',
-				'br',
-				'strong',
-				'em',
-				'u',
-				's',
-				'strike',
-				'del',
-				'h1',
-				'h2',
-				'h3',
-				'h4',
-				'h5',
-				'h6',
-				'ul',
-				'ol',
-				'li',
-				'a',
-				'blockquote',
-				'pre',
-				'code',
-				'span',
-				'div',
-			],
-			ALLOWED_ATTR: ['href', 'title', 'target', 'rel'],
-			ALLOWED_URI_REGEXP:
-				/^(?:(?:(?:f|ht)tps?|mailto|tel|callto|sms|cid|xmpp):|[^a-z]|[a-z+\-.]+(?:[^a-z+.\-:]|$))/i,
-		})
-
-		return sanitized
+		return DOMPurify.sanitize(html, DOMPURIFY_CONFIG)
 	}, [html])
 
 	// If no HTML content, return null
